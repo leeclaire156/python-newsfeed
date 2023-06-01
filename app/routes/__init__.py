619c9dd54055de0c1003df1717dc62478f860f54
Claire Lee <lee.claire156@gmail.com>
2023-06-01 19:16:24 -0400

    Adds dashboard routes

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,3 +1,4 @@
 # The .home syntax directs the program to find the module (aka single file) named 'home' in the current directory
 # Then we import the bp object from it, but we rename it 'home'
 from .home import bp as home
+from .dashboard import bp as dashboard
