0a7ae00911b996a44008937d3b7a1cbcb6b45029
Claire Lee <lee.claire156@gmail.com>
2023-06-02 16:53:36 -0400

    Adds notes to db init file

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-
+# Newsfeed
 
 
 * "Activate Scripts cannot be loaded because running 
 scripts is disabled on this system" Error Fix by [Rahul](https://tecadmin.net/author/myadmin/) on [TecAdmin](https://tecadmin.net/):<br></br>https://tecadmin.net/powershell-running-scripts-is-disabled-system/#:~:text=To%20resolve%20the%20%E2%80%9CRunning%20scripts,select%20%E2%80%9CRun%20as%20administrator%E2%80%9DO
+
+* getenv method from os information from [GeeksforGeeks](https://www.geeksforgeeks.org/):<br></br>https://www.geeksforgeeks.org/python-os-getenv-method/
+
+* load_dotenv() function from dotenv information provided by [Davos](https://stackoverflow.com/users/1335793/davos) on [Stack Overflow](https://stackoverflow.com/):<br></br>https://stackoverflow.com/questions/41546883/what-is-the-use-of-python-dotenv
--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,11 +1,21 @@
+# getenv() function is part of Python's built-in os module
+# returns the value of the environment variable key (in this case, we specified we want the value of the 'DB_URL' key) if it exists otherwise returns the default value.
 from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+# dotenv works for local development, load_dotenv() reads the file provided as a file path
 from dotenv import load_dotenv
 
+# load_dotenv() will set the environment variables from .env and we access with os module
 load_dotenv()
 
-# connect to database using env variable
+
+# Below code connects to database using env variable
+
+# The engine variable manages the overall connection to the database.
+# In production, DB_URL will be a proper environment variable
 engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
+# The Session variable generates temporary connections for performing create, read, update, and delete (CRUD) operations.
 Session = sessionmaker(bind=engine)
+# The Base class variable helps us map the models to real MySQL tables.
 Base = declarative_base()
