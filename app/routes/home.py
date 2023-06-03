0abd7490222f2bfea725914c028eb9712a763ad5
Claire Lee <lee.claire156@gmail.com>
2023-06-03 14:03:49 -0400

    Renders posts to homepage

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from app.models import Post
+from app.db import get_db
 # Blueprint() lets us consolidate routes onto a single 'bp' object that the parent app can register later.
 # This corresponds to using the Router middleware of Express.js
 bp = Blueprint('home', __name__, url_prefix='/')
@@ -6,7 +8,24 @@ bp = Blueprint('home', __name__, url_prefix='/')
 # render_template() function responds with a template instead of a string.
 @bp.route('/')
 def index():
-  return render_template('homepage.html')
+  # get all posts
+  # get_db() function returns a session connection that's tied to this route's context
+  db = get_db()
+  # We use query() method on the connection object (db) to query the Post model and save all that in the posts variable
+  # Multiline query below, single line version: posts = db.query(Post).order_by(Post.created_at.desc()).all()
+  # MUST use parentheses in python for multiline queries, unlike in JS
+  posts = (
+    db
+      .query(Post)
+      .order_by(Post.created_at.desc())
+      .all()
+  )
+  
+  # returning homepage WITH the posts
+  return render_template(
+  'homepage.html',
+  posts=posts
+)
 
 @bp.route('/login')
 def login():
