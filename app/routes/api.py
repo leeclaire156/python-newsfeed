4ddb35c7f1c8f77f97565ada08a77dfbc08db306
Claire Lee <lee.claire156@gmail.com>
2023-06-05 13:08:57 -0400

    Creates signup API route

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
 
@@ -8,6 +8,19 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 @bp.route('/users', methods=['POST'])
 def signup():
   data = request.get_json()
-  print(data)
+  db = get_db()
 
-  return ''
+  # create a new user
+  newUser = User(
+    username = data['username'],
+    email = data['email'],
+    password = data['password']
+  )
+
+  # saves new user in database
+  # use the db.add() method to prep the INSERT statement and the db.commit() method to officially update the database
+  db.add(newUser)
+  db.commit()
+
+  # Returns new user's id to the front end
+  return jsonify(id = newUser.id)
